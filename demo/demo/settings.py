619387f41f5c3dae10a502207774cedeaf8797f2
RonakNandanwar26 <ronaknandanwar10@gmail.com>
2021-06-09 10:19:09 +0530

    random commit

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -180,5 +180,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ronaknandanwar10@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ron@kPiyu1999'
+EMAIL_HOST_USER = '**** Email ****'
+EMAIL_HOST_PASSWORD = '**** Password ****'
